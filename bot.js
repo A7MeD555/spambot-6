5ed9904a6b3060f645a21807de54d8f55f7b66f9
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-11-18 23:18:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ var prefix = "6";
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', message => {
 
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+  if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+});
 
 
 
